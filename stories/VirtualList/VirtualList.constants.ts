2345194eac909f136c3e32d17ca98b3e3fbcc050
Alexey Filatov <alexeyfilatov994@gmail.com>
2023-05-11 18:41:23 +0300

    fix: test ci

--- stories/VirtualList/VirtualList.constants.ts
+++ stories/VirtualList/VirtualList.constants.ts
@@ -1,5 +1,6 @@
 export const DEFAULT_MIN_HEIGHT = 60;
 export const DEFAULT_BUFFER_SIZE = 5;
+
 export const ROOT_ELEMENT = 'virtual-view-root';
 export const CHILD_ELEMENT = 'virtual-view-child';
 export const DATASET_ID = 'list_item_idx';
